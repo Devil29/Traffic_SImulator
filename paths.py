496495c191437dd3d29ad36d8b244126be7871b1
Vishal <vishalraj48@gmail.com>
2015-09-24 16:44:37 -0700

    Adding stored paths

---
+++ paths.py
@@ -0,0 +1,9 @@
+from math import *
+
+f=open('anglelist.txt','r')
+g=open('pathlist.txt','r')
+anglelist=f.read().split('\n');
+pathlist=g.read().split('\n');
+#for i in pathlist:
+
+
--- simu.py
+++ simu.py
@@ -1,14 +1,22 @@
 from math import *
 from random import random
-f=open('inp.txt')
+f=open('inp.txt','r')
+g=open('output.txt','w')
 previous_angle=0
-count=0
+count=1
+path_angle=[]
 data=f.read().split()
+path_angle.append(data[0])
 for i in range(len(data)):
     if(i%10==0 and i>9):
             previous_angle=(float(data[i-10])+float(data[i-9])+float(data[i-8]))/3
             if(abs(previous_angle-float(data[i]))>8):
                 count=count+1;
+                path_angle.append(data[i])
     #print str(abs(previous_angle-float(data[i])))+'\t'+str(previous_angle)+'\t'+str(data[i])+'\t'+str(i)
-print count;
+g.write(str(count))
+g.write('\n')
+g.write(str(path_angle))
+f.close()
+g.close()
         
