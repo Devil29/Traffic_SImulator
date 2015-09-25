a4fc1413a7627c111b85a154208c11dfef31505e
Vishal <vishalraj48@gmail.com>
2015-09-24 19:27:28 -0700

    Included the curves on paths

--- Tester.py
+++ Tester.py
@@ -3,10 +3,19 @@ from random import random
 r=open('input.txt','r')
 f=open('inp.txt','w')
 t=int(r.readline())
-for i in range(t):
-    readings=r.readline().split(' ',1)
-    b,c=int(readings[0]),int(readings[1])
-    for i in range(b*10):   
-        f.write(str(c+(random()*2)-1)+"\n")
+previous_angle=0
+for ii in range(t):
+    readings=r.readline().split(' ')
+    b,c,d=int(readings[0]),int(readings[1]),int(readings[2])
+    for i in range(b*10):
+        if(d==0):
+            #f.write(str(c+(random()*2)-1)+"    "+str(previous_angle)+"\n")
+            f.write(str(c+(random()*2)-1)+"\n")
+            previous_angle=c
+        elif(d==1 and ii==0):
+            previous_angle=c
+        else:
+            f.write(str(previous_angle+(random()*2)-1+(c*i/(b*10)))+"\n")
+            #f.write(str(previous_angle+(random()*2)-1+(c*i/(b*10)))+"    "+str(previous_angle)+" HERE---\n")
 f.close()
 r.close()
---
+++ intefer.py
@@ -0,0 +1,7 @@
+from math import *
+from random import random
+f=open('i.txt','w')
+for i in range(18600):
+    f.write(str(i+1)+'\n')
+f.close()
+
